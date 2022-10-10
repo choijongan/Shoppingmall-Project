45e3e9b40c914961f5ff68a3e7f5abfe7b654aa5
choijongan <mmm7134@naver.com>
2022-10-10 20:26:51 +0900

    로그인, hnm로고

--- hnm-react-router/src/component/Navbar.js
+++ hnm-react-router/src/component/Navbar.js
@@ -6,13 +6,18 @@ const Navbar = () => {
   return (
     <div>
         <div>
-        <FontAwesomeIcon icon={faUser} />
+            <div class="login-button">
+                <FontAwesomeIcon icon={faUser} />
+                <div>로그인</div>
+            </div>
+        </div>
+        <div className='nav-section'>
+            <img 
+            width={100}
+            src='https://brandlogos.net/wp-content/uploads/2014/10/h-m-logo.png'/>
         </div>
         <div>
-
-        </div>
-        <div>
-
+            
         </div>
     </div>
   )
