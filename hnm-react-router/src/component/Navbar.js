147b78352af3c0cae1e5f96b66ac0120bb96741c
choijongan <mmm7134@naver.com>
2022-10-10 21:36:33 +0900

    상단 메뉴 만들기

--- hnm-react-router/src/component/Navbar.js
+++ hnm-react-router/src/component/Navbar.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+    const menuList = [
+        '여성', 
+        'Divided', 
+        '남성',
+        '신생아/유아',
+        '아동',
+        'H&M Home',
+        'Sale',
+        '지속가능성',
+    ]
   return (
     <div>
         <div>
@@ -16,8 +27,16 @@ const Navbar = () => {
             width={100}
             src='https://brandlogos.net/wp-content/uploads/2014/10/h-m-logo.png'/>
         </div>
-        <div>
-            
+        <div className='menu-area'>
+                <ul className='menu-list'>
+                    {menuList.map(menu=> (
+                    <li>{menu}</li>
+                    ))}
+                </ul>
+            <div>
+                <FontAwesomeIcon icon={faSearch}/>
+                <input type="text"/>
+            </div>
         </div>
     </div>
   )
