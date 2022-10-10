7e98c26adadad89454001074ed7590155388daf8
choijongan <mmm7134@naver.com>
2022-10-10 19:55:57 +0900

    리액트 FontAwewome npm설치

--- hnm-react-router/src/App.js
+++ hnm-react-router/src/App.js
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
 
 
 //1. 전체상품페이지, 로그인페이지, 상품상세페이지
+//1-1. 네비게이션 바
 //2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다.
 //3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
 //3-1. 상품디테일을 눌렀으나, 비로그인시 로그인페이지가 나온다.
@@ -17,6 +19,7 @@ import ProductDetail from './page/ProductDetail';
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<ProductAll/>}/>
         <Route path="/login" element={<Login/>}/>
