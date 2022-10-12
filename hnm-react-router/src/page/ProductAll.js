d3d65c1293086a112e8c16aa9eb062fa133889bc
choijongan <mmm7134@naver.com>
2022-10-12 19:35:28 +0900

    넣은 데이터 불러오기

--- hnm-react-router/src/page/ProductAll.js
+++ hnm-react-router/src/page/ProductAll.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const ProductAll = () => {
+
+  const getProducts= async ()=>{
+    let url = `http://localhost:5000/products`
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  }
+  useEffect(()=>{
+      getProducts()
+   },[])
   return (
     <div>
         
