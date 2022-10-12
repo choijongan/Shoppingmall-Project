f6abd89ca3e6f2e100fe73ff3e1f1cb5033ac1a6
choijongan <mmm7134@naver.com>
2022-10-12 19:47:52 +0900

    ProductCard만들어서 불러오기

---
+++ hnm-react-router/src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='//lp2.hm.com/hmgoepprod?set=source[/b3/10/b3107100df8deecc3c86264d9ad99daeb847e620.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'/>
+        <div>Conscious choice</div>
+        <div>집탑 리브니트 스웨터</div>
+        <div>₩39900</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- hnm-react-router/src/page/ProductAll.js
+++ hnm-react-router/src/page/ProductAll.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
-
+  const [productList, setProductList] = useState([]);
   const getProducts= async ()=>{
     let url = `http://localhost:5000/products`
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    setProductList(data)
   }
   useEffect(()=>{
       getProducts()
    },[])
   return (
     <div>
-        
+        <ProductCard/>
     </div>
   )
 }
